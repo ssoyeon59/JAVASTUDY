e931cee1da02e3341e21fa5b878b38065fbb1d45
강소연 <nebula059@naver.com>
2022-09-29 23:20:26 +0900

    필드 선언, 생성자 공부한것 남아있어서 추가함

---
+++ JAVAStudy/src/java6_3/Car.java
@@ -0,0 +1,9 @@
+package java6_3;
+
+public class Car {
+
+    Car(String color, int cc){
+
+    }
+
+}
---
+++ JAVAStudy/src/java6_3/CarExam.java
@@ -0,0 +1,11 @@
+package java6_3;
+
+public class CarExam {
+
+    public static void main(String[] args) {
+        Car myCar = new Car ("검정", 3000);
+        //Car myCar = new Car(); //기본생성자 호출할 수 없음
+        //이미 Car(String color, int cc)라고 Car클래스에 선어이 되어있음
+    }
+
+}
