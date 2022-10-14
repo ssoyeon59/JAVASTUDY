df4b8ce946bddca01812ad15da71762e63d91624
강소연 <nebula059@naver.com>
2022-10-15 01:40:31 +0900

    chap10 공부한것

---
+++ JAVAStudy/chap10/src/sec02/exam02/ThrowsExample.java
@@ -0,0 +1,15 @@
+//package sec02.exam02;
+//
+//public class ThrowsExample {
+//    public static void main(String[] args) throws ClassNotFoundException {
+//        try {
+//            findClass();
+//        } catch (ClassNotFoundException e) {
+//            System.out.println("클래스가 존재하지 않습니다");
+//        }
+//
+//        public static void findClass() throw ClassNotFoundException {
+//            Class clazz = Class.forName("java.lang.String2");
+//        }
+//    }
+//}
